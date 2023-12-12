491046492a26cd9d771c6d69a519e6f1da5b9f99
lechinhbao <lechinhbao3477@gmail.com>
2023-12-12 15:32:21 +0700

    port

--- Sever/app.js
+++ Sever/app.js
@@ -73,6 +73,9 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 4000);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
